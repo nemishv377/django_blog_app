939639bf4c36ecd1a4a519512509d3f2f13e5995
Nemish Vaghasiya <nemishv.tagline@gmail.com>
2024-11-29 18:49:11 +0530

    Added authentication templates and static files

--- django_blog_app/settings.py
+++ django_blog_app/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Or the path where your static files are stored
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
