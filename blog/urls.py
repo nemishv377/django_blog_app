beb4e7bef081077f87d2bd4aa1826b423715abf3
Nemish Vaghasiya <nemishv.tagline@gmail.com>
2024-12-09 09:27:10 +0530

    Added author and blog details view and templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('blogs/', views.blogs_list, name='blogs_list'),
     path('<int:id>/', views.blog_detail, name='blog_detail'),
+    path('bloggers/', views.bloggers_list, name='bloggers_list'),
+    path('bloggers/<int:id>/', views.blogger_detail, name='blogger_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from accounts.models import Author
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -14,3 +15,12 @@ def blogs_list(request):
 def blog_detail(request, id):
   blog = get_object_or_404(Blog, id=id)
   return render(request, 'blog/blog_detail.html', {'blog': blog})
+
+def bloggers_list(request):
+  bloggers = Author.objects.all()
+  return render(request, 'blog/bloggers_list.html', {'bloggers': bloggers})
+  
+def blogger_detail(request, id):
+  author = get_object_or_404(Author, id=id)
+  blogs = Blog.objects.filter(author=author).order_by('-created_at')
+  return render(request, 'blog/blogger_detail.html', {'author': author, 'blogs': blogs})
